57bb1c3a2f93720e2e3bd540222e3781b9016074
tanp4n4maa00-beep <tanp4n4maa00@gmail.com>
2026-04-20 11:35:25 +0800

    Add files via upload

---
+++ tugas 2.py
@@ -0,0 +1,6 @@
+angka = float(input("masukkan angka: "))
+
+while angka <= -0.1:
+    print("harus positif!!!")
+    angka = float(input("masukkan angka: "))
+    print(f"angka yang di temukan adalah: {angka}")
